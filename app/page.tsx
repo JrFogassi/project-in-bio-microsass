89d893d85b0c92ed6e8258b1c9a864d867e2c1cd
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-01-24 16:14:03 -0300

    Creating Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
+import Hero from "./components/landing-page/hero";
 
 export default function Home() {
-  return <div className="bg-background-primary">Morgana, Eu te amo!</div>
+  return (
+  <div className="max-w-7xl mx-auto">
+    <div className="text-white">Morgana, eu te amo!</div>
+    <Hero />
+
+    {/*<Header />   
+    <VideoExplanation />
+    <Pricing />
+    <FAQ /> */}
+  </div>
+  );
 }
