c7ce1bc19179957a7524a0cbb5f19e9556051b53
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-01-24 16:52:04 -0300

    Creating button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Hero from "./components/landing-page/hero";
+import Hero from "./components/landing-page/Hero";
 
 export default function Home() {
   return (
