b805769769571565525ad0fb113af7ab5a1dc294
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-01-24 21:36:44 -0300

    Creating text area and text input components

--- app/components/ui/Text_Area.tsx
+++ app/components/ui/Text_Area.tsx
@@ -7,8 +7,9 @@ export default function TextArea(
         <textarea 
             {...props}
             className={cn(
-                'w-full p-3 bg-background'
+                'w-full p-3 bg-background text-white placeholder:text-content-placeholder rounded-wl border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary',
+                props.className
             )}
-            />
-        );
+        />
+    );
 }
